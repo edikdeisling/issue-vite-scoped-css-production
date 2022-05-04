e9820099c3478d8461d0640e13bfaa9585f9fc12
Eduard Deisling <edikdeisling@gmail.com>
2022-05-04 11:15:08 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld class="parent-class" />
+</template>
+
+<style scoped>
+  .parent-class {
+    background-color: red;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <template v-if="true">
+    <div v-if="true">Child</div>
+  </template>
+</template>
+
+<style scoped></style>
